2702aa949ad279661081ed02c4bbf7be7d8a6242
admin <admin@YD02-09>
2023-08-24 17:45:58 +0900

    메인페이지

---
+++ final01/src/main/java/co/yedam/app/common/main/mapper/MainMapper.java
@@ -0,0 +1,12 @@
+package co.yedam.app.common.main.mapper;
+
+import java.util.List;
+
+import co.yedam.app.common.main.service.MainVO;
+
+public interface MainMapper {
+
+	public List<MainVO> selectProdAmtList();
+	
+	
+}
---
+++ final01/src/main/java/co/yedam/app/common/main/service/MainServiceImpl.java
@@ -0,0 +1,23 @@
+package co.yedam.app.common.main.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.yedam.app.common.main.mapper.MainMapper;
+
+
+@Service
+public class MainServiceImpl implements MainService{
+
+	@Autowired
+	MainMapper mainMapper;
+	
+	@Override
+	public List<MainVO> selectProdAmtList() {
+		
+		return mainMapper.selectProdAmtList();
+	}
+
+}
